fd7375573cbe0a14fc077412407d955d38fbf17e
Darren Midkiff <d.claymidkiff@gmail.com>
2015-07-29 21:42:26 -0500

    split coord tracking in to trackmove.lua

--- digRoom.lua
+++ digRoom.lua
@@ -1,19 +1,13 @@
 -- Dcmid's Room Builder
 
--- Depends on genDig
+-- Depends on trackMove
 
 -- Make entries relative to turtle's position.
 --   The X direction runs from the turtle's left to its right
 --   The Y direction runs from bedrock to the sky
 --   The Z direction runs from behind the turtle through the front of it\n
 
-os.loadAPI("genDig")
-
-xCoord = 0
-yCoord = 0
-zCoord = 0
---orientation relative to starting position. 0 = forward, 1 = right, 2 = backward, 3 = left
-orientation = 0
+os.loadAPI("trackMove")
 
 write("Left or Right (l/r): ")
 lrDir = read()
@@ -33,69 +27,16 @@ write("Height (Y): ")
 height = tonumber(read())
 write("Depth (Z): ")
 depth = tonumber(read())
-turnRight = lrDir == "r"
+shouldTurnRight = lrDir == "r"
 
 function shouldReturn()
-  return turtle.getItemCount(16) > 0 or turtle.getFuelLevel() <= math.abs(xCoord) + math.abs(yCoord) + math.abs(zCoord)
-end
-
-function adjustOrientation(change)
-  orientation = orientation + change
-  if orientation > 3 then
-    orientation = orientation - 4
-  elseif orientation < 0 then
-    orientation = orientation + 4
-  end
-  return true
-end
-
-function orientTo(dir)
-  if dir < 0 or dir > 3 then
-    print("dir out of bounds")
-    error()
-  end
-  if dir == orientation - 1 or (orientation == 0 and dir == 3) then
-    turtle.turnLeft()
-    adjustOrientation(-1)
-  else
-    while orientation ~= dir do
-      turtle.turnRight()
-      adjustOrientation(1)
-    end
-  end
-  return true
+  return turtle.getItemCount(16) > 0 or turtle.getFuelLevel() <= math.abs(trackMove.xCoord) + math.abs(trackMove.yCoord) + math.abs(trackMove.zCoord)
 end
 
 function returnToOrigin()
-  if yCoord > 0 then
-    while yCoord > 0 do
-      genDig.down()
-      yCoord = yCoord - 1
-    end
-  elseif yCoord < 0 then
-    while yCoord < 0 do
-      genDig.up()
-      yCoord = yCoord + 1
-    end
-  end
-  if xCoord > 0 then
-    orientTo(3)
-    while xCoord > 0 do
-      genDig.mine()
-      xCoord = xCoord - 1
-    end
-  elseif xCoord < 0 then
-    orientTo(1)
-    while xCoord < 0 do
-      genDig.mine()
-      xCoord = xCoord + 1
-    end
-  end
-  orientTo(2)
-  while zCoord > 0 do
-    genDig.mine()
-    zCoord = zCoord - 1
-  end
+  trackMove.gotoY(0)
+  trackMove.gotoX(0)
+  trackMove.gotoZ(0)
   return true
 end
 
@@ -106,56 +47,36 @@ for i = 1, height do
         returnToOrigin()
         return
       end
-      genDig.mine()
-      if orientation == 0 then
-        zCoord = zCoord + 1
-      elseif orientation == 2 then
-        zCoord = zCoord - 1
+      trackMove.mine()
+      if trackMove.orientation ~= 0 and trackMove.orientation ~= 2 then
       else
         print("turtle facing wrong way 1")
         return
       end
     end
     if j ~= width then
-      if turnRight then
-        turtle.turnRight()
-        adjustOrientation(1)
-        genDig.mine()
-        if orientation == 1 then
-          xCoord = xCoord + 1
-        else
-          xCoord = xCoord - 1
-        end
-        turtle.turnRight()
-        adjustOrientation(1)
+      if shouldTurnRight then
+        trackMove.turnRight()
+        trackMove.mine()
+        trackMove.turnRight()
       else
-        turtle.turnLeft()
-        adjustOrientation(-1)
-        genDig.mine()
-        if orientation == 1 then
-          xCoord = xCoord + 1
-        else
-          xCoord = xCoord - 1
-        end
-        turtle.turnLeft()
-        adjustOrientation(-1)
+        trackMove.turnLeft()
+        trackMove.mine()
+        trackMove.turnLeft()
       end
-      turnRight = not turnRight
+      shouldTurnRight = not shouldTurnRight
     end
   end
-  genDig.turnAround()
-  adjustOrientation(2)
+  trackMove.turnAround()
   if i < height then
     if udDir == "u" then
-      genDig.up()
-      yCoord = yCoord + 1
+      trackMove.up()
     else
-      genDig.down()
-      yCoord = yCoord - 1
+      trackMove.down()
     end
   end
 end
-print(xCoord)
-print(yCoord)
-print(zCoord)
+print(trackMove.xCoord)
+print(trackMove.yCoord)
+print(trackMove.zCoord)
 returnToOrigin()
---
+++ trackMove.lua
@@ -0,0 +1,140 @@
+os.loadAPI("genDig")
+
+xCoord = 0
+yCoord = 0
+zCoord = 0
+--orientation relative to starting position. 0 = forward, 1 = right, 2 = backward, 3 = left
+orientation = 0
+
+function adjustOrientation(change)
+  orientation = orientation + change
+  if orientation > 3 then
+    orientation = orientation - 4
+  elseif orientation < 0 then
+    orientation = orientation + 4
+  end
+  return true
+end
+
+function orientTo(dir)
+  if dir < 0 or dir > 3 then
+    print("dir out of bounds")
+    error()
+  end
+  if dir == orientation - 1 or (orientation == 0 and dir == 3) then
+    turtle.turnLeft()
+    adjustOrientation(-1)
+  else
+    while orientation ~= dir do
+      turtle.turnRight()
+      adjustOrientation(1)
+    end
+  end
+  return
+end
+
+function down()
+  genDig.down()
+  yCoord = yCoord - 1
+  return
+end
+
+function up()
+  genDig.up()
+  yCoord = yCoord + 1
+  return
+end
+
+function mine()
+  genDig.mine()
+  if orientation == 0 then
+    zCoord = zCoord + 1
+  elseif orientation == 1 then
+    xCoord = xCoord + 1
+  elseif orientation == 2 then
+    zCoord = zCoord - 1
+  else
+    xCoord = xCoord - 1
+  end
+  return
+end
+
+function mineFor(numBlocks)
+  for i = 1, numBlocks do
+    genDig.mine()
+  end
+  if orientation == 0 then
+    zCoord = zCoord + numBlocks
+  elseif orientation == 1 then
+    xCoord = xCoord + numBlocks
+  elseif orientation == 2 then
+    zCoord = zCoord - numBlocks
+  else
+    xCoord = xCoord - numBlocks
+  end
+  return
+end
+
+function turnAround()
+  genDig.turnAround()
+  adjustOrientation(2)
+  return
+end
+
+function turnRight()
+  turtle.turnRight()
+  adjustOrientation(1)
+  return
+end
+
+function turnLeft()
+  turtle.turnLeft()
+  adjustOrientation(-1)
+  return
+end
+
+function gotoX(pos)
+  if xCoord > pos then
+    orientTo(3)
+    while xCoord > pos do
+      mine()
+    end
+  end
+  elseif xCoord < pos then
+    orientTo(1)
+    while xCoord < pos do
+      mine()
+    end
+  end
+  return
+end
+
+function gotoY(pos)
+  if yCoord > pos then
+    while yCoord > pos do
+      down()
+    end
+  end
+  elseif yCoord < pos then
+    while yCoord < pos do
+      up()
+    end
+  end
+  return
+end
+
+function gotoZ(pos)
+  if zCoord > pos then
+    orientTo(2)
+    while zCoord > pos do
+      mine()
+    end
+  end
+  elseif zCoord < pos then
+    orientTo(0)
+    while zCoord < pos do
+      mine()
+    end
+  end
+  return
+end
